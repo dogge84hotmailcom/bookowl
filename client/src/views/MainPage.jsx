e33bdca24e44f3dc9e8d4e81599c4b5f85bb289d
dogge84@hotmail.com <dogge84@hotmail.com>
2026-07-09 16:14:57 +0200

    Added Mainpage with search input and navigation

--- client/src/views/MainPage.jsx
+++ client/src/views/MainPage.jsx
@@ -1,6 +1,33 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import Footer from "../components/Footer.jsx"
+
+
 export default function MainPage (){
 
+    const [searchInput, setSearchInput] = useState("")
+    const navigate = useNavigate();
+
+
+    function handleSearch(e) {
+        e.preventDefault()
+
+        navigate(`/searchresults?q=${searchInput}`)
+
+
+
+
+    }
+
     return (
+        <>
         <p>Testing MainPage</p>
+        <form onSubmit={(e) => handleSearch(e)}>
+            <input value={searchInput} onChange={(event) => setSearchInput(event.target.value)} type="text" placeholder="Search for a title..."></input>
+            <button type="submit">Search</button>
+        </form>
+        <Footer />
+
+        </>
     )
 }
