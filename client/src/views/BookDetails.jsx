e7803146cac5bad6d20a4aec52018d3912bee9d4
dogge84@hotmail.com <dogge84@hotmail.com>
2026-07-20 16:10:06 +0200

    bookdetails view with api fetch and rendering

--- client/src/views/BookDetails.jsx
+++ client/src/views/BookDetails.jsx
@@ -1,6 +1,69 @@
+import { useParams } from "react-router-dom";
+import {useState, useEffect} from "react"
+
+
 export default function BookDetails (){
 
+    const {id} = useParams();
+
+    const [book, setBook] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+
+        async function getBookDetails(){
+
+            setBook(null)
+            setLoading(true)
+            setError(null)
+
+            try{
+                const response = await fetch(`http://localhost:3000/api/books/${id}`)
+
+                if(!response.ok){
+                    throw new Error("Something went wrong.")
+                }
+                const data = await response.json()
+                console.log(data)
+
+                if(data.error){
+                    throw new Error(data.error.message)
+                }
+
+                setBook(data)
+                setLoading(false)
+            }
+
+            catch(err){
+            setError(err.message)
+            setLoading(false)
+    
+            }
+        }
+
+        getBookDetails()
+
+    }, [id])
+
+    
+
     return (
+        <div>
+
         <p>Testing BookDetails page</p>
+        {loading && <p>Loading book...</p>}
+        {error && <p>{error}</p>}
+        {book && 
+        <div>
+        <h1>{book.volumeInfo.title}</h1>
+        <p>{book.volumeInfo.subtitle}</p>
+        <h2>{book.volumeInfo.authors?.[0]}</h2>
+        <p>{book.volumeInfo.publishedDate}</p>
+        <p>{book.volumeInfo.description}</p>
+        <img src={book.volumeInfo.imageLinks?.thumbnail} alt={book.volumeInfo.title}/>
+        </div>}
+        
+        </div>
     )
 }
