b51cb14535d9403a0f9586fe999ea7b51240f5e8
dogge84@hotmail.com <dogge84@hotmail.com>
2026-07-23 15:56:14 +0200

    sql database setup and pool created in server file

---
+++ server/db.js
@@ -0,0 +1,13 @@
+import { Pool } from "pg"
+import "dotenv/config"
+
+const pool = new Pool({
+
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD
+})
+
+export default pool
