e303f94c7330801b69a49271b8a50595484dc6ea
dogge84@hotmail.com <dogge84@hotmail.com>
2026-09-01 12:26:02 +0200

    change import directory for App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,26 @@
-
-import { Routes, Route } from 'react-router-dom'
-import Header from "./components/header.jsx"
-import Footer from "./components/Footer.jsx"
-import BookDetails from "./views/BookDetails.jsx"
-import MainPage from "./views/MainPage.jsx"
-import SavedBooks from "./views/SavedBooks.jsx"
-import SearchResults from "./views/SearchResults.jsx"
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import BookDetails from "./views/BookDetails.jsx";
+import MainPage from "./views/MainPage.jsx";
+import SavedBooks from "./views/SavedBooks.jsx";
+import SearchResults from "./views/SearchResults.jsx";
 
 function App() {
-
-  
-
   return (
     <>
-    <Header />
-    <main role="main">
-      <Routes>
-      <Route path="/" element={<MainPage />}/>
-      <Route path="/searchresults" element={<SearchResults />}/>
-      <Route path="/bookdetails/:id" element={<BookDetails />}/>
-      <Route path="/savedbooks" element={<SavedBooks />}/>
-      </Routes>
-    </main>
-    <Footer />
+      <Header />
+      <main role="main">
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/searchresults" element={<SearchResults />} />
+          <Route path="/bookdetails/:id" element={<BookDetails />} />
+          <Route path="/savedbooks" element={<SavedBooks />} />
+        </Routes>
+      </main>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
